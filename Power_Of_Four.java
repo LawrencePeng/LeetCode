1fad62600e543385ddb0cb46854e0f31d854317d
Lawrence <13128923967@163.com>
2016-06-29 02:42:18 +0800

    add Power_Of_Four

---
+++ Power_Of_Four.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        return num == 1
+        || num == 4
+        || num == 16
+        || num == 64
+        || num == 256
+        || num == 1024
+        || num == 4096
+        || num == 16384
+        || num == 65536
+        || num == 262144
+        || num == 1048576
+        || num == 4194304
+        || num == 16777216
+        || num == 67108864
+        || num == 268435456
+        || num == 1073741824;
+    }
+}
