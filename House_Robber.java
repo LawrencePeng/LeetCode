4b8e41bcb4c805463c06444b0f66c81b6ba545f5
Lawrence <lawrencepeng233@gmail.com>
2017-02-09 15:41:42 +0800

    add House_Robber

---
+++ House_Robber.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int rob(int[] num) {
+        int prevNo = 0;
+        int prevYes = 0;
+
+        for (int n : num) {
+            int temp = prevNo;
+            prevNo = Math.max(prevNo, prevYes);
+            prevYes = n + temp;
+        }
+
+        return Math.max(prevNo, prevYes);
+}
+}
