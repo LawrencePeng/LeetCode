325b641f87302bb8f4810b0802082f1b29c64bf4
Lawrence <lawrencepeng233@gmail.com>
2017-02-18 18:57:39 +0800

    add Add Strings

---
+++ Add_Strings.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(eval(num1 + " + "+ num2))
+
