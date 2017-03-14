d52b6acd8228b6a4de8f4565f6412dc7f391ebd7
Lawrence <lawrencepeng233@gmail.com>
2017-03-14 16:57:15 +0800

    add Ones And Zeros

---
+++ Ones_And_Zeroes.java
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        dp = [[0] * (n+1) for _ in range(m+1)]
+
+        def count(s):
+            return sum(1 for c in s if c == '0'), sum(1 for c in s if c == '1')
+
+        for z, o in [count(s) for s in strs]:
+            for x in range(m, -1, -1):
+                for y in range(n, -1, -1):
+                    if x >= z and y >= o:
+                        dp[x][y] = max(1 + dp[x-z][y-o], dp[x][y])
+
+        return dp[m][n]
