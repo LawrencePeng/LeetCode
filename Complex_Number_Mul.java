386494f8e7d73cafbf1e87085f1d1065a77e68ee
Lawrence <lawrencepeng233@gmail.com>
2017-03-27 19:48:04 +0800

    add Complex Num Mul

---
+++ Complex_Number_Mul.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public String complexNumberMultiply(String a, String b) {
+        int[] coefs1 = Stream.of(a.split("\\+|i")).mapToInt(Integer::parseInt).toArray(),
+          coefs2 = Stream.of(b.split("\\+|i")).mapToInt(Integer::parseInt).toArray();
+        return (coefs1[0]*coefs2[0] - coefs1[1]*coefs2[1]) + "+" + (coefs1[0]*coefs2[1] + coefs1[1]*coefs2[0]) + "i";
+    }
+}
